f86c47899006a6b3677e6b3ddad051e9fc4ea7aa
b2ycompany <b2ylion@gmail.com>
2025-03-21 09:58:23 -0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,23 +1,19 @@
+// lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage"
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAyoX1YqAdqHuIvSoj0Yw_FYnhCBv-KfEA",
-  authDomain: "fht-sistema.firebaseapp.com",
-  projectId: "fht-sistema",
-  storageBucket: "fht-sistema.appspot.com", // ✅ Corrigido aqui
-  messagingSenderId: "583837273524",
-  appId: "1:583837273524:web:1583addfce581e61ac76e1",
-  measurementId: "G-6FJ2H3G32N"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const auth = getAuth(app)
-const db = getFirestore(app)
-const storage = getStorage(app)
 
-export { app, auth, db, storage }
+export const auth = getAuth(app)
+export const db = getFirestore(app)
